a197798d9bf1ba0a2164a2e8baf4870188208970
Vijay Viswanathan <vijayv@andrew.cmu.edu>
2022-11-15 11:53:36 -0500

    Add option for k-means++ initialization in the k-means algorithm

--- clustering/active_clustering.py
+++ clustering/active_clustering.py
@@ -5,7 +5,18 @@
 python active_clustering.py --dataset OPIEC59k --data-path \
     /projects/ogma1/vijayv/okb-canonicalization/clustering/data \
     --dataset-split test \
-    --num_clusters 490
+    --num_clusters 490 \
+    --num_seeds 5
+
+python active_clustering.py --dataset OPIEC59k \
+    --data-path /projects/ogma1/vijayv/okb-canonicalization/clustering/data \
+    --dataset-split test \
+    --num_clusters 490 \
+    --num_seeds 1 \
+    --normalize-vectors \
+    --split-normalization \
+    --init kmeans++ \
+    --verbose |& tee ~/logs/canon/opiec_clustering_simplified_kmeanspp.log
 '''
 
 from sklearn import metrics
@@ -48,7 +59,9 @@ parser.add_argument('--num_clusters', type=int, default=3)
 parser.add_argument('--max-feedback-given', type=int, default=10, help="Number of instances of user feedback (e.g. oracle queries) allowed")
 parser.add_argument('--num_seeds', type=int, default=10)
 parser.add_argument('--feature_extractor', type=str, choices=["identity", "BERT", "TFIDF"], default="identity")
-parser.add_argument('--normalize-vectors', action="store_true", help="")
+parser.add_argument('--normalize-vectors', action="store_true", help="Normalize vectors")
+parser.add_argument('--split-normalization', action="store_true", help="Normalize per-view components separately (for multi-view clustering)")
+parser.add_argument('--init', type=str, choices=["random", "kmeans++"], default="random", help="Initialization algorithm to use for k-means.")
 parser.add_argument('--verbose', action="store_true")
 
 
@@ -69,10 +82,10 @@ def sample_cluster_seeds(features, labels, num_seed_points_per_label = 1, aggreg
             labels.append(y_value)
     return np.array(labels)
 
-def cluster(semisupervised_algo, features, labels, num_clusters, max_feedback_given=None):
+def cluster(semisupervised_algo, features, labels, num_clusters, init="random", max_feedback_given=None, normalize_vectors=False, split_normalization=False, verbose=False):
     assert semisupervised_algo in ["KMeans", "PCKMeans", "ConstrainedKMeans", "SeededKMeans"]
     if semisupervised_algo == "KMeans":
-        clusterer = KMeans(n_clusters=num_clusters)
+        clusterer = KMeans(n_clusters=num_clusters, normalize_vectors=normalize_vectors, split_normalization=split_normalization, init=init, verbose=verbose)
         clusterer.fit(features)
     elif semisupervised_algo == "PCKMeans":
         oracle = ExampleOracle(labels, max_queries_cnt=max_feedback_given)
@@ -84,24 +97,36 @@ def cluster(semisupervised_algo, features, labels, num_clusters, max_feedback_gi
         clusterer = PCKMeans(n_clusters=num_clusters)
         clusterer.fit(features, ml=pairwise_constraints[0], cl=pairwise_constraints[1])
     elif semisupervised_algo == "ConstrainedKMeans":
-        clusterer = ConstrainedKMeans(n_clusters=num_clusters)
+        clusterer = ConstrainedKMeans(n_clusters=num_clusters, init=init)
         cluster_seeds = sample_cluster_seeds(features, labels)
         clusterer.fit(features, y=cluster_seeds)
     elif semisupervised_algo == "SeededKMeans":
-        clusterer = SeededKMeans(n_clusters=num_clusters)
+        clusterer = SeededKMeans(n_clusters=num_clusters, init=init)
         cluster_seeds = sample_cluster_seeds(features, labels)
         clusterer.fit(features, y=cluster_seeds) 
     else:
         raise ValueError(f"Algorithm {semisupervised_algo} not supported.")
     return clusterer
 
-def compare_algorithms(features, labels, side_information, num_clusters, dataset_name, max_feedback_given=None, algorithms=["KMeans", "PCKMeans", "ConstrainedKMeans", "SeededKMeans"], num_seeds=3, verbose=True, normalize_vectors=False):
+def compare_algorithms(features, labels, side_information, num_clusters, dataset_name, max_feedback_given=None, algorithms=["KMeans", "PCKMeans", "ConstrainedKMeans", "SeededKMeans"], num_seeds=3, verbose=True, normalize_vectors=False, split_normalization=False, init="random"):
     algo_results = defaultdict(list)
     timer = time.perf_counter()
 
     if normalize_vectors:
-        features = normalize(features, axis=1, norm="l2")
+        if verbose:
+            print(f"Starting feature normalization.")
+        if split_normalization:
+            timer = time.perf_counter()
+            kg_features = normalize(features[:, :300], axis=1, norm="l2")
+            bert_features = normalize(features[:, 300:], axis=1, norm="l2")
+            features = np.hstack([kg_features, bert_features])
+        else:
+            features = normalize(features, axis=1, norm="l2")
+        if verbose:
+            print(f"Feature normalization took {round(time.perf_counter() - timer, 3)} seconds.")
 
+    if verbose:
+        print(f"Starting comparison of {num_seeds} seeds:")
     for i, seed in enumerate(range(num_seeds)):
         if verbose:
             print(f"Starting experiments for {i}th seed")
@@ -110,22 +135,18 @@ def compare_algorithms(features, labels, side_information, num_clusters, dataset
             if verbose:
                 print(f"Running {semisupervised_algo} for seed {seed}")
             start_time = time.perf_counter()
-            clusterer = cluster(semisupervised_algo, features, labels, num_clusters, max_feedback_given=max_feedback_given)
+            clusterer = cluster(semisupervised_algo, features, labels, num_clusters, max_feedback_given=max_feedback_given, normalize_vectors=normalize_vectors, split_normalization=split_normalization, init=init, verbose=verbose)
             elapsed_time = time.perf_counter() - start_time
             if verbose:
                 print(f"Took {round(elapsed_time, 3)} seconds to cluster points.")
             metric_dict = {}
             algo_results[semisupervised_algo].append(metric_dict)
             if dataset_name == "OPIEC59k":
-                side_information.p.use_assume = True
                 ave_prec, ave_recall, ave_f1, macro_prec, micro_prec, pair_prec, macro_recall, micro_recall, \
-                pair_recall, macro_f1, micro_f1, pair_f1, model_clusters, model_Singletons, gold_clusters, gold_Singletons \
+                pair_recall, macro_f1, micro_f1, pairwise_f1, model_clusters, model_Singletons, gold_clusters, gold_Singletons \
                     = cluster_test(side_information.p, side_information.side_info, clusterer.labels_, side_information.true_ent2clust, side_information.true_clust2ent)
 
                 # Compute Macro/Macro/Pairwise F1 on OPIEC59k
-                macro_f1 = np.nan
-                micro_f1 = np.nan
-                pairwise_f1 = np.nan
                 metric_dict["macro_f1"] = macro_f1
                 metric_dict["micro_f1"] = micro_f1
                 metric_dict["pairwise_f1"] = pairwise_f1
@@ -162,7 +183,19 @@ if __name__ == "__main__":
     X, y, side_information = load_dataset(args.dataset, args.data_path, args.dataset_split)
     assert set(y) == set(range(len(set(y))))
     features = extract_features(X, args.feature_extractor, args.verbose)
-    algorithms=["KMeans", "PCKMeans", "ConstrainedKMeans", "SeededKMeans"]
-    results = compare_algorithms(features, y, side_information, args.num_clusters, args.dataset, max_feedback_given=args.max_feedback_given, num_seeds=args.num_seeds, verbose=args.verbose, normalize_vectors=args.normalize_vectors)
+    #algorithms=["KMeans", "PCKMeans", "ConstrainedKMeans", "SeededKMeans"]
+    algorithms=["KMeans"]
+    results = compare_algorithms(features,
+                                 y,
+                                 side_information,
+                                 args.num_clusters,
+                                 args.dataset,
+                                 max_feedback_given=args.max_feedback_given,
+                                 num_seeds=args.num_seeds,
+                                 verbose=args.verbose,
+                                 normalize_vectors=args.normalize_vectors,
+                                 split_normalization = args.split_normalization,
+                                 algorithms=algorithms,
+                                 init=args.init)
     summarized_results = summarize_results(results)
     print(json.dumps(summarized_results, indent=2))
