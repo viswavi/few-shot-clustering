edfa002f1be0cd02adc70526ad57164e572721d9
Vijay Viswanathan <vijayv@andrew.cmu.edu>
2022-11-16 11:20:51 -0500

    Add an easier subset of 20NG to our experiments

--- clustering/dataloaders.py
+++ clustering/dataloaders.py
@@ -16,7 +16,10 @@ def preprocess_20_newsgroups(per_topic_samples = 100, shuffle=True, topics=None)
     for target, text in zip(newsgroups['target'], newsgroups['data']):
         if topics is None or target_to_name[target] in topics:
             group_by_topics[target].append(text)
-    all_data = [(text, target) for target in group_by_topics for text in group_by_topics[target][:per_topic_samples]]
+    if per_topic_samples is None:
+        all_data = [(text, target) for target in group_by_topics for text in group_by_topics[target]]
+    else:
+        all_data = [(text, target) for target in group_by_topics for text in group_by_topics[target][:per_topic_samples]]
     if shuffle:
         random.shuffle(all_data)
     text, labels = zip(*all_data)
@@ -29,15 +32,22 @@ def reorder_labels(label_array):
     return [label_to_new_label_mapping[l] for l in label_array]
 
 def load_dataset(dataset_name, data_path, dataset_split=None):
-    assert dataset_name in ["iris", "20_newsgroups_all", "20_newsgroups_sim3", "OPIEC59k"]
+    assert dataset_name in ["iris", "20_newsgroups_all", "20_newsgroups_full", "20_newsgroups_sim3", "20_newsgroups_diff3", "OPIEC59k"]
     if dataset_name == "iris":
         samples, gold_cluster_ids = datasets.load_iris(return_X_y=True)
         side_information = None
     elif dataset_name == "20_newsgroups_all":
-        samples, gold_cluster_ids = preprocess_20_newsgroups()
+        samples, gold_cluster_ids = preprocess_20_newsgroups(per_topic_samples=100)
+        side_information = None
+    elif dataset_name == "20_newsgroups_full":
+        samples, gold_cluster_ids = preprocess_20_newsgroups(per_topic_samples=None)
         side_information = None
     if dataset_name == "20_newsgroups_sim3":
         samples, gold_cluster_ids = preprocess_20_newsgroups(topics=["comp.graphics", "comp.os.ms-windows.misc", "comp.windows.x"])
+        gold_cluster_ids = reorder_labels(gold_cluster_ids)
+        side_information = None
+    if dataset_name == "20_newsgroups_diff3":
+        samples, gold_cluster_ids = preprocess_20_newsgroups(topics=["alt.atheism", "rec.sport.baseball", "sci.space"])
         gold_cluster_ids = reorder_labels(gold_cluster_ids)
         side_information = None
     elif dataset_name == "OPIEC59k":
